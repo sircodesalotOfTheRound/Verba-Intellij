722c37545789026c54c61eb09677952c3388db57
Reuben Kuhnert <sircodesalot@gmail.com>
2014-11-21 13:48:06 -0600

    Recommit

---
+++ src/MyAction.java
@@ -0,0 +1,19 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.verba.language.build.buildtools.Build;
+import com.verba.language.graph.symbols.table.entries.SymbolTableEntry;
+import org.apache.xerces.util.SymbolTable;
+
+/**
+ * Created by sircodesalot on 14/11/21.
+ */
+public class MyAction extends AnAction {
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    Build build = Build.fromString("fn function() { }");
+
+    for (SymbolTableEntry entry : build.symbolTable().entries()) {
+      System.out.println(entry.fqn());
+    }
+  }
+}
